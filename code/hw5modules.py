11bff7390783e59aa25e0fc2c80d6631cd136d82
Vuochlang <72162972+Vuochlang@users.noreply.github.com>
2021-03-23 19:39:09 -0700

    clean up code and wrote the write up

--- code/hw5modules.py
+++ code/hw5modules.py
@@ -1,4 +1,5 @@
 import mysql_table as c
+import sql_queries as query
 
 
 # create all of your relations
@@ -9,3 +10,8 @@ def create_relations(name, password, database):
 # insert and parse the data from the provided CSV file into your relations
 def insert_data(file_name, name, password, database):
     c.insert_data(file_name, name, password, database)
+
+
+# query command for part 2
+def command(question_number, name, password, database):
+    query.command(question_number, name, password, database)
--- code/main.py
+++ code/main.py
@@ -1,12 +1,11 @@
 import hw5modules as hw5
-import sql_queries as q
 
 if __name__ == '__main__':
     # create all of your relations
-    # hw5.create_relations('root', 'YueLan!_2388', '351_a5')
+    hw5.create_relations('root', 'YueLan!_2388', '351_a5')
 
     # insert and parse the data from the provided CSV file into your relations
-    # hw5.insert_data('tmdb_5000_movies.csv', 'root', 'YueLan!_2388', '351_a5')
+    hw5.insert_data('tmdb_5000_movies.csv', 'root', 'YueLan!_2388', '351_a5')
 
     # query
-    q.command(5, 'root', 'YueLan!_2388', '351_a5')
+    hw5.command(3, 'root', 'YueLan!_2388', '351_a5')
